4c7275b7807e7c0d63f3c5587ab3c7f0f353deda
Malintha Adikari <malinthasa@gmail.com>
2017-11-22 16:01:09 +0530

    Improving code

--- algorithms/bruteforceimpl.py
+++ algorithms/bruteforceimpl.py
@@ -5,8 +5,20 @@ import numpy as np
 import os.path
 import matplotlib.pyplot as plt
 
+# Configurations
+number_of_iterations = 500
+weights = np.array([8,5,4,3,1])
+# TODO 0 is appended to the tail of the weight array to store original indexes
+weights = np.insert(weights,len(weights),0)
+# Generating random weights
+# weights = np.random.uniform(low=1, high=10, size=(5,))
+results = np.zeros((number_of_iterations,))
+# prepare weights in descending order
+get_top_criterias = calculations.get_indexes_descending(weights, 6)
+
 # reading data
 df = datareading.read_data(os.path.join(os.path.split(__file__)[0], "../resources/data/HouseDetails.csv"))
+
 # Pre-processing data
 normalized_df = datapreprocess.normalize_data(df)
 processed_df = datapreprocess.adjust_for_weights(normalized_df)
@@ -16,45 +28,35 @@ processed_df.insert(5, 'ID', range(0, len(df)))
 house_matrix = calculations.get_matrix(processed_df)
 number_of_decisions = len(house_matrix)
 
-results = np.zeros((500,))
+# Select Top 10 decisions using Weighted Model Method
+weighted_attributes = calculations.get_weighted_attribute_matrix(house_matrix, weights)
+sum = weighted_attributes.sum(axis=1)
+# Get top 10 decisions
+get_top_decisions = calculations.get_indexes_descending(sum, 10)
 
-filter_levels= [90,75,55,35,10]
-
-for iterator in range(0,500):
-    # Generating random weights
-    weights = np.random.uniform(low=1, high=10, size=(5,))
-    # fix weights set
-    # weights = np.array([8,5,4,3,1])
-    # TODO 0 is appended to the tail of the weight array to store original indexes
-    weights = np.insert(weights,len(weights),0)
-    weighted_attributes = calculations.get_weighted_attribute_matrix(house_matrix, weights)
-    sum = weighted_attributes.sum(axis=1)
-    # Get top 10 decisions
-    get_top_decisions = calculations.get_indexes_descending(sum, 10)
-
-    # prepare weights in descending order
-    get_top_criterias = calculations.get_indexes_descending(weights, 6)
-
+# Select Top 10 decisions using brute force method
+for iterator in range(0,number_of_iterations):
     # Copy of house matrix for constructing new filtering matrix
     constructed_house_matrix = house_matrix
 
+    # Generating filtering criteria for 5 attributes
+    filter_levels = calculations.generate_filtering_criteria(5, 90)
+    # Iterating through each attribute
     for i in range(len(weights)-1):
         # Get current feature column
         current_feature = constructed_house_matrix[:,get_top_criterias[i]]
         # sort it and get indexes in descending order
-        # TODO fix this magical number 18. it is the total-needed rows. rest rows are deleted
-        # here we remove 18 from the tail in each iteration
-        selected_decisions = np.squeeze(np.asarray(current_feature)).argsort()[::-1][number_of_decisions-(i+1)*18:]
-        # manually picking filtering levels
-        # selected_decisions = np.squeeze(np.asarray(current_feature)).argsort()[::-1][filter_levels[i]:]
-        constructed_house_matrix = np.delete(constructed_house_matrix, (selected_decisions), axis=0)
+        selected_decisions = np.squeeze(np.asarray(current_feature)).argsort()[::-1][-filter_levels[i]:]
+        constructed_house_matrix = np.delete(constructed_house_matrix, selected_decisions, axis=0)
 
     # constructed_house_matrix[:,5] contains top 10 decisions found in brute force algorithm
     # get_top_decisions contains top 10 decisions found from standard weight based method
 
+    # Performance calculation
     number_of_correct_decisions = np.sum(np.in1d(constructed_house_matrix[:,5], get_top_decisions)==True)
     performance = number_of_correct_decisions / 10 *100
     results[iterator] = performance
 
+# Visualization
 plt.plot(results)
 plt.show()
--- datamanupulator/calculations.py
+++ datamanupulator/calculations.py
@@ -1,4 +1,5 @@
-
+import random
+import numpy
 
 def get_matrix(house_df):
     return house_df.as_matrix(columns=None)
@@ -9,3 +10,7 @@ def get_weighted_attribute_matrix(house_matrix, weights):
 
 def get_indexes_descending(object, numberofindexes):
     return object.argsort()[::-1][:numberofindexes]
+
+def generate_filtering_criteria(n, total):
+    dividers = sorted(random.sample(range(1, total), n - 1))
+    return numpy.asarray([a - b for a, b in zip(dividers + [total], [0] + dividers)])
