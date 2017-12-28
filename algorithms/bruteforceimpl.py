e79398af212081df25dccb89dd48ae2ad70f49b5
Malintha <malinthasa@gmail.com>
2017-12-28 19:15:01 +0530

    Improvements

--- algorithms/bruteforceimpl.py
+++ algorithms/bruteforceimpl.py
@@ -5,8 +5,13 @@ import numpy as np
 import os.path
 import matplotlib.pyplot as plt
 
+filtering_combinations = np.asarray(list(calculations.generate_all_combinations(5,90)))
+reporting = filtering_combinations
+
 # Configurations
-number_of_iterations = 500
+number_of_iterations = len(filtering_combinations)
+print("Number of combinations: ", number_of_iterations)
+# Selecting random set of weights
 weights = np.array([8,5,4,3,1])
 # TODO 0 is appended to the tail of the weight array to store original indexes
 weights = np.insert(weights,len(weights),0)
@@ -36,30 +41,35 @@ get_top_decisions = calculations.get_indexes_descending(sum, 10)
 
 # Select Top 10 decisions using brute force method
 for iterator in range(0,number_of_iterations):
+    print("Current iteration: ", iterator)
     # Copy of house matrix for constructing new filtering matrix
     constructed_house_matrix = house_matrix
 
     # Generating filtering criteria for 5 attributes
-    filter_levels = calculations.generate_filtering_criteria(5, 90)
+    # We prepare a list of numbers foe selecting values from each column ramdomly
+    # total of the selection is equal to 90 values
+    # filter_levels = calculations.generate_filtering_criteria(5, 90)
     # Iterating through each attribute
     for i in range(len(weights)-1):
         # Get current feature column
         current_feature = constructed_house_matrix[:,get_top_criterias[i]]
         # sort it and get indexes in descending order
-        selected_decisions = np.squeeze(np.asarray(current_feature)).argsort()[::-1][-filter_levels[i]:]
-        constructed_house_matrix = np.delete(constructed_house_matrix, selected_decisions, axis=0)
+        if filtering_combinations[iterator][i] !=0:
+            selected_decisions = np.squeeze(np.asarray(current_feature)).argsort()[::-1][-filtering_combinations[iterator][i]:]
+            constructed_house_matrix = np.delete(constructed_house_matrix, selected_decisions, axis=0)
 
     # constructed_house_matrix[:,5] contains top 10 decisions found in brute force algorithm
     # get_top_decisions contains top 10 decisions found from standard weight based method
 
     # Performance calculation
     number_of_correct_decisions = np.sum(np.in1d(constructed_house_matrix[:,5], get_top_decisions)==True)
-    performance = number_of_correct_decisions / 10 *100
+    performance = number_of_correct_decisions * 10
     results[iterator] = performance
+    np.hstack([reporting[iterator],performance])
 
 # Visualization
 plt.plot(results)
 plt.xlabel('Iteration')
 plt.ylabel('Performance %')
 plt.show()
-
+np.savetxt("results.csv", reporting, delimiter=",")
--- datamanupulator/calculations.py
+++ datamanupulator/calculations.py
@@ -14,3 +14,11 @@ def get_indexes_descending(object, numberofindexes):
 def generate_filtering_criteria(n, total):
     dividers = sorted(random.sample(range(1, total), n - 1))
     return numpy.asarray([a - b for a, b in zip(dividers + [total], [0] + dividers)])
+
+def generate_all_combinations(n,total):
+    if n == 1:
+        yield (total,)
+    else:
+        for i in xrange(total + 1):
+            for j in generate_all_combinations(n - 1,total - i):
+                yield (i,) + j
